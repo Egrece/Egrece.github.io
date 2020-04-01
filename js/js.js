e2ccaee6b4e5f0f81b837f966c04d1af77e8ebcd
Eriken Gjeta <e.gjeta@se19.qmul.ac.uk>
2020-04-01 18:38:25 +0100

    removed menu button

--- js/js.js
+++ js/js.js
@@ -1,21 +1,21 @@
 let menu = document.getElementById('menu');
 let nav = document.getElementById('nav');
-nav.hidden = true;
+//nav.hidden = true;
 let icon = document.getElementById('menuicon')
 
 
-menu.onclick = function() {
-  if (nav.hidden) {
-    nav.hidden = false;
-  } else {
-    nav.hidden = true;
-  }
-}
+//menu.onclick = function() {
+//  if (nav.hidden) {
+//    nav.hidden = false;
+//  } else {
+//    nav.hidden = true;
+//  }
+//}
 
-icon.onclick = function() {
-    if (nav.hidden) {
-    nav.hidden = false;
-  } else {
-    nav.hidden = true;
-  }
-}
+//icon.onclick = function() {
+//    if (nav.hidden) {
+//    nav.hidden = false;
+//  } else {
+//    nav.hidden = true;
+//  }
+//}
