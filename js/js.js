4365d83942bff15fdf4bd3d8938d90b45f5be0b3
Eriken Gjeta <e.gjeta@se19.qmul.ac.uk>
2020-04-01 16:41:39 +0100

    perfecto!

--- js/js.js
+++ js/js.js
@@ -1,11 +1,19 @@
 let menu = document.getElementById('menu');
 let nav = document.getElementById('nav');
-let header = document.getElementsByTagName('HEADER')[0];
-nav.hidden =true;
+nav.hidden = true;
+let icon = document.getElementById('menuicon')
 
 
 menu.onclick = function() {
   if (nav.hidden) {
+    nav.hidden = false;
+  } else {
+    nav.hidden = true;
+  }
+}
+
+icon.onclick = function() {
+    if (nav.hidden) {
     nav.hidden = false;
   } else {
     nav.hidden = true;
